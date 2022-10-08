0a5e5720a136a6f6146265f0d1cf7dc61dafd84e
chunleili <li_cl@foxmail.com>
2022-10-08 23:44:18 +0800

    init

---
+++ CS106/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Sat Oct 8 23:25:37 2022"
+#define SPL_BUILD_USER "cl"
+
+#endif
