f978d3f5af61536f1c3c114de62c63ae41803584
chunleili <li_cl@foxmail.com>
2022-10-09 20:53:43 +0800

    1

--- assign1/assign1-starter/perfect.cpp
+++ assign1/assign1-starter/perfect.cpp
@@ -56,31 +56,63 @@ void findPerfects(long stop) {
  * header comment.
  */
 long smarterSum(long n) {
-    /* TODO: Fill in this function. */
-    return 0;
+    if(n==1) return 0;
+    long total = 1;
+    for (long divisor = 2; divisor*divisor <= n; divisor++) {
+        if (n % divisor == 0) {
+            if(divisor==n/divisor)
+                total += divisor;
+            else
+                total += divisor + n/divisor;
+        }
+    }
+    return total;
 }
 
 /* TODO: Replace this comment with a descriptive function
  * header comment.
  */
 bool isPerfectSmarter(long n) {
-    /* TODO: Fill in this function. */
-    return false;
+    return (n != 0) && (n == smarterSum(n));
 }
 
 /* TODO: Replace this comment with a descriptive function
  * header comment.
  */
 void findPerfectsSmarter(long stop) {
-     /* TODO: Fill in this function. */
+    for (long num = 1; num < stop; num++) {
+        if (isPerfectSmarter(num)) {
+            cout << "Found perfect number: " << num << endl;
+        }
+        if (num % 10000 == 0) cout << "." << flush; // progress bar
+    }
+    cout << endl << "Done searching up to " << stop << endl;
+}
+
+
+bool isPrime(long n)
+{
+    return smarterSum(n)==1;
 }
 
 /* TODO: Replace this comment with a descriptive function
  * header comment.
  */
 long findNthPerfectEuclid(long n) {
-    /* TODO: Fill in this function. */
-    return 0;
+    int cnt=0;
+    int k=1;
+    while(true)
+    {
+        int m=pow(2,k) -1 ;
+        if(isPrime(m))
+        {
+            cnt++;
+            if(cnt==n)
+                return pow(2,k-1) * (pow(2,k) -1);
+        }
+        k++;
+    }
+
 }
 
 
@@ -123,4 +155,53 @@ PROVIDED_TEST("Time trials of findPerfects on doubling input sizes") {
 }
 
 // TODO: add your student test cases here
+//STUDENT_TEST("Time of findPerfect")
+//{
+//    TIME_OPERATION(10000, findPerfects(10000));
+//    TIME_OPERATION(20000, findPerfects(20000));
+//    TIME_OPERATION(40000, findPerfects(40000));
+//    TIME_OPERATION(80000, findPerfects(80000));
+//    TIME_OPERATION(160000,findPerfects(160000));
+//    TIME_OPERATION(320000,findPerfects(320000));
+//}
 
+STUDENT_TEST("NEGATIVE")
+{
+    EXPECT(!isPerfect(-1233));
+    EXPECT(!isPerfect(-123123));
+}
+
+
+//STUDENT_TEST("Confirm smarterSum of small inputs") {
+//    EXPECT_EQUAL(smarterSum(1), 0);
+//    EXPECT_EQUAL(smarterSum(6), 6);
+//    EXPECT_EQUAL(smarterSum(12), 16);
+//    EXPECT_EQUAL(smarterSum(25), divisorSum(25));
+//    EXPECT_EQUAL(smarterSum(27), divisorSum(27));
+//    EXPECT_EQUAL(smarterSum(81), divisorSum(81));
+//    for(int i=1; i<1000; i++)
+//    {
+//        EXPECT_EQUAL(smarterSum(i), divisorSum(i));
+//    }
+//}
+
+
+//STUDENT_TEST("Time of findPerfect")
+//{
+//        TIME_OPERATION(10000, findPerfects(10000));
+//        TIME_OPERATION(20000, findPerfects(20000));
+//        TIME_OPERATION(40000, findPerfects(40000));
+//        TIME_OPERATION(80000, findPerfects(80000));
+//        TIME_OPERATION(160000,findPerfects(160000));
+
+//    TIME_OPERATION(10000, findPerfectsSmarter(10000));
+//    TIME_OPERATION(20000, findPerfectsSmarter(20000));
+//    TIME_OPERATION(40000, findPerfectsSmarter(40000));
+//    TIME_OPERATION(80000, findPerfectsSmarter(80000));
+//    TIME_OPERATION(160000,findPerfectsSmarter(160000));
+//}
+
+STUDENT_TEST("find Nth Euclidean")
+{
+    EXPECT_EQUAL( findNthPerfectEuclid(3), 496);
+}
